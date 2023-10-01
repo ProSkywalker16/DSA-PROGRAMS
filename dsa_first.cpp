57223218145eb07433505f010a864bdfb50a19a0
ProSkywalker16 <chaudhuripromit16@gmail.com>
2023-10-01 21:08:52 +0530

    modified:   dsa_first.cpp modified:   output/dsa_first.exe

--- dsa_first.cpp
+++ dsa_first.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 int stack[100], n = 100, top = -1;
-void push(int val)
+void push(int value)
 {
     if (top >= n - 1)
         cout << "Stack Overflow" << endl;
     else
     {
         top++;
-        stack[top] = val;
+        stack[top] = value;
     }
 }
 void pop()
